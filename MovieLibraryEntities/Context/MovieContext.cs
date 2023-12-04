da51a2cd921d0c269f79567852f6b89cce7e3c66
Julia Zblewski <jzblewski2@my.wctc.edu>
2023-12-04 13:23:17 -0600

    fixed

---
+++ MovieLibraryEntities/Context/MovieContext.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MovieLibraryEntities.Models;
+
+namespace MovieLibraryEntities.Context;
+
+public class MovieContext : DbContext
+{
+    private readonly ILogger<MovieContext> _logger;
+    public DbSet<Genre> Genres { get; set; }
+    public DbSet<MovieGenre> MovieGenres { get; set; }
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<Occupation> Occupations { get; set; }
+    public DbSet<UserMovie> UserMovies { get; set; }
+    public DbSet<User> Users { get; set; }
+    public MovieContext()
+    {
+            
+    }
+
+    public MovieContext(ILogger<MovieContext> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        optionsBuilder
+            .LogTo(action => _logger.LogInformation(action), LogLevel.Information)
+            //.EnableSensitiveDataLogging()
+            .UseSqlServer(configuration.GetConnectionString("MovieContext")
+            );
+    }
+}
